4e8509c188cf4188e75d667b55a28e5c770c54d6
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 18:53:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,13 @@ def badge_maker(name)
   print badge
   badge
 end
+
+def assign_rooms(speakers)
+  room_list = []
+
+  speakers.each_with_index do |speaker_name, index|
+    room_list << "Hello, #{speaker_name}! You'll be assigned to room #{index + 1}!"
+  end
+
+  room_list
+end
