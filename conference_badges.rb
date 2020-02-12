5d14243fddb02631dec69c3264353582454267e2
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 18:57:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,3 +19,8 @@ def assign_rooms(speakers)
 
   room_list
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_rooms(attendees).each {|room| puts room}
+end
